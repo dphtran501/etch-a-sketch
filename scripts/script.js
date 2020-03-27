54aa30744820f0bb6a43c2e5e40d70d23f479d22
Derek <dchang501101@gmail.com>
2020-03-27 00:00:30 -0700

    Add clear button and footer
    Also add setGrid() function to create the etch-a-sketch grid with the
    provided number of columns and rows.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,38 @@
-const MAX_ROWS = 16;
-const MAX_COLUMNS = 16;
+const DEFAULT_MAX_ROWS = 16;
+const DEFAULT_MAX_COLUMNS = 16;
 
 const gridContainer = document.querySelector(".grid-container");
+const clearButton = document.querySelector('#clear-button');
 
-for (let r = 1; r <= MAX_ROWS; r++){
-    for (let c = 1; c <= MAX_COLUMNS; c++){
-        let gridSquare = document.createElement("div");
-        gridSquare.setAttribute("grid-column", Number(c));
-        gridSquare.setAttribute("grid-row", Number(r));
-        gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', onMouseOver);
-        gridContainer.appendChild(gridSquare);
+clearButton.addEventListener('click', onClearClick);
+
+setGrid(DEFAULT_MAX_COLUMNS, DEFAULT_MAX_ROWS);
+
+function setGrid(numberOfColumns, numberOfRows) {
+    // Set grid container
+    gridContainer.style.gridTemplateColumns = "repeat(" + numberOfColumns + ", minmax(" + gridContainer.clientWidth/numberOfColumns + "px, 1fr))";
+    gridContainer.style.gridTemplateRows = "repeat(" + numberOfRows + ", minmax(" + gridContainer.clientHeight/numberOfRows + "px, 1fr))";
+    // Add grid items (squares)
+    for (let r = 1; r <= numberOfRows; r++){
+        for (let c = 1; c <= numberOfColumns; c++){
+            let gridSquare = document.createElement("div");
+            gridSquare.setAttribute("grid-column", c);
+            gridSquare.setAttribute("grid-row", r);
+            gridSquare.classList.add('grid-square');
+            gridSquare.addEventListener('mouseover', onMouseOver);
+            gridContainer.appendChild(gridSquare);
+        }
     }
 }
 
 function onMouseOver(e) {
     let gridSquare = e.target;
     gridSquare.classList.add('grid-square--mouseover');
+}
+
+function onClearClick(e) {
+    let gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach((square) => {
+        square.classList.remove('grid-square--mouseover');
+    })
 }
